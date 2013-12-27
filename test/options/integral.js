4f8b0cc2f7ef2f43d0711b485aa9190195cf34fe
Roman Komarov <kizmarh@ya.ru>
2013-12-27 19:36:32 +0400

    Fixing the integral test for #147

--- test/options/integral.js
+++ test/options/integral.js
@@ -11,32 +11,12 @@ describe('integral test', function() {
         this.shouldBeEqual('integral.css', 'integral.expected.css');
     });
 
-    it.skip('Should detect everything in integral test', function(done) {
-        var config = this.comb.getConfig('csscomb');
-        var input = this.readFile('integral.css');
-        var expected = {
-            'remove-empty-rulesets': true,
-            'always-semicolon': true,
-            'color-case': 'lower',
-            'color-shorthand': true,
-            'element-case': 'lower',
-            'leading-zero': false,
-            quotes: 'single',
-            'strip-spaces': true,
-            'eof-newline': true,
-            'stick-brace': '\n',
-            'colon-space': ['', ' '],
-            'combinator-space': [' ', ' '],
-            'rule-indent': '    ',
-            'block-indent': '    ',
-            'unitless-zero': true,
-            'vendor-prefix-align': true
-        };
-
+    it('Should detect everything in integral test', function() {
+        var input = this.readFile('integral.expected.css');
+        // Clone the required config object, otherwise other tests would fail
+        var expected = JSON.parse(JSON.stringify(this.comb.getConfig('csscomb')));
+        delete expected['sort-order'];
+        delete expected['exclude'];
         this.shouldDetect(undefined, input, expected);
-
-        expected['sort-order'] = config['sort-order'];
-        this.comb.configure(expected);
-        this.shouldDetect(undefined, 'integral.css', expected);
     });
 });
