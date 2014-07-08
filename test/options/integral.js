519255acd8c439b3de3ef49823645515a4672d89
Tony Ganch <tonyganch@gmail.com>
2014-07-08 20:10:03 +0400

    Add test for #252

--- test/options/integral.js
+++ test/options/integral.js
@@ -11,6 +11,12 @@ describe('integral test', function() {
         this.shouldBeEqual('integral.css', 'integral.expected.css');
     });
 
+    it('Issue 252', function() {
+        var config = this.Comb.getConfig('csscomb');
+        this.comb.configure(config);
+        this.shouldBeEqual('issue-252.sass', 'issue-252.expected.sass');
+    });
+
     it('Should detect everything in integral test', function() {
         var input = this.readFile('integral.expected.css');
         // Clone the required config object, otherwise other tests would fail
