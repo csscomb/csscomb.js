878c8500df74ff511bfa64fc2dd77463411906be
Vladimir Pavlikov <theitzamna@gmail.com>
2014-08-28 23:17:45 +0400

    implementing more tests, added preceeding search

--- test/options/rule-delimiter-scss.js
+++ test/options/rule-delimiter-scss.js
@@ -12,4 +12,29 @@ describe('options/rule-delimiter (scss)', function() {
         this.comb.configure({ 'rule-delimiter': "\n\n\n" });
         this.shouldBeEqual('single.scss', 'single.expected.scss');
     });
+
+    it('Should insert blank lines before comment #2 and #3', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('comment.scss', 'comment.expected.scss');
+    });
+
+    it('Should insert nlank line before .home and after @media', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('stylesheet.scss', 'stylesheet.expected.scss');
+    });
+
+    it('Should insert newline before i but do not touch include', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('include.scss', 'include.expected.scss');
+    });
+
+    it('Should insert blank line before em', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('preceeded.scss', 'preceeded.expected.scss');
+    });
+
+    it('Very complex test', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('complex.scss', 'complex.expected.scss');
+    });
 });
