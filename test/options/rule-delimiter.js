89c1249a9e444a9749cdba83621f09df29d416a4
Vladimir Pavlikov <theitzamna@gmail.com>
2014-08-29 20:09:28 +0400

    remove comments from test, fixing test file for css

--- test/options/rule-delimiter.js
+++ test/options/rule-delimiter.js
@@ -5,36 +5,26 @@ describe('options/rule-delimiter (css)', function() {
 
     it('Should add blank line before .some-class', function() {
         this.comb.configure({ 'rule-delimiter': "\n\n" });
-        this.shouldBeEqual('fontface.less', 'fontface.expected.css');
+        this.shouldBeEqual('fontface.css', 'fontface.expected.css');
     });
 
     it('Should change nothing', function() {
         this.comb.configure({ 'rule-delimiter': "\n\n\n" });
-        this.shouldBeEqual('single.less', 'single.expected.css');
+        this.shouldBeEqual('single.css', 'single.expected.css');
     });
 
     it('Should insert blank lines before .some-class and after .another-class', function() {
         this.comb.configure({ 'rule-delimiter': "\n\n" });
-        this.shouldBeEqual('importfile.less', 'importfile.expected.css');
+        this.shouldBeEqual('importfile.css', 'importfile.expected.css');
     });
 
     it('Should insert blank line before h1', function() {
         this.comb.configure({ 'rule-delimiter': "\n\n" });
-        this.shouldBeEqual('media.less', 'media.expected.css');
+        this.shouldBeEqual('media.css', 'media.expected.css');
     });
 
     it('Should insert newline before .another-class', function() {
         this.comb.configure({ 'rule-delimiter': "\n\n" });
         this.shouldBeEqual('test.css', 'test.expected.css');
-    });
-
-    it('Should insert blank line before em', function() {
-        this.comb.configure({ 'rule-delimiter': "\n\n" });
-        this.shouldBeEqual('preceeded.less', 'preceeded.expected.less');
-    });
-
-    it('Very complex test', function() {
-        this.comb.configure({ 'rule-delimiter': "\n\n" });
-        this.shouldBeEqual('complex.less', 'complex.expected.less');
     });
 });
