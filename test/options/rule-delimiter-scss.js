1284b5e0efa5ba5652b7c80e87df9fd5e2b52f19
ognia <ms.ognia@gmail.com>
2014-08-29 21:13:17 +0400

    tests update

--- test/options/rule-delimiter-scss.js
+++ test/options/rule-delimiter-scss.js
@@ -20,12 +20,22 @@ describe('options/rule-delimiter (scss)', function() {
 
     it('Should insert nlank line before .home and after @media', function() {
         this.comb.configure({ 'rule-delimiter': "\n\n" });
-        this.shouldBeEqual('stylesheet.scss', 'stylesheet.expected.scss');
+        this.shouldBeEqual('media.scss', 'media.expected.scss');
     });
 
     it('Should insert newline before i but do not touch include', function() {
         this.comb.configure({ 'rule-delimiter': "\n\n" });
         this.shouldBeEqual('include.scss', 'include.expected.scss');
+    });
+
+    it('Should insert newline before .some-class', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('import.scss', 'import.expected.scss');
+    });
+
+    it('Should change nothing', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('extend.scss', 'extend.expected.scss');
     });
 
     it('Should insert blank line before em', function() {
@@ -41,5 +51,35 @@ describe('options/rule-delimiter (scss)', function() {
     it('Should insert blank line before n-th child', function() {
         this.comb.configure({ 'rule-delimiter': "\n\n" });
         this.shouldBeEqual('for.scss', 'for.expected.scss');
+    });
+
+    it('Should insert blank line after @if and after @else', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('if.scss', 'if.expected.scss');
+    });
+
+    it('Should insert blank line before @return', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('return.scss', 'return.expected.scss');
+    });
+
+    it('Should insert blank line before @mixin', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('mixin.scss', 'mixin.expected.scss');
+    });
+
+    it('Should insert blank line after @function', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('function.scss', 'function.expected.scss');
+    });
+
+    it('Should add new lines after variables and before .sub_element', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('variable.scss', 'variable.expected.scss');
+    });
+
+    it('Should add new lines before @at-root', function() {
+        this.comb.configure({ 'rule-delimiter': "\n\n" });
+        this.shouldBeEqual('atroot.scss', 'atroot.expected.scss');
     });
 });
