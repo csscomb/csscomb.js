776228f649a0bd0ac76599ad048b5d612c6aa7b4
Tony Ganch <tonyganch@gmail.com>
2015-01-22 16:19:11 +0300

    [tests] Move test files to test dirs
    Update Mocha to v1.20

---
+++ test/options/remove-empty-rulesets-less/test.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+
+describe('options/remove-empty-rulesets (less):', function() {
+    it('Issue 201. Test 1', function() {
+        this.comb.configure({ 'remove-empty-rulesets': true });
+        this.shouldBeEqual('1.less', '1.expected.less');
+    });
+
+    it('Issue 201. Test 2', function() {
+        this.comb.configure({ 'remove-empty-rulesets': true });
+        var string = '#a {#b {} #d {}}';
+        assert.equal(this.comb.processString(string, { syntax: 'less' }), '');
+    });
+
+    it('Issue 201. Test 3', function() {
+        this.comb.configure({
+            'remove-empty-rulesets': false,
+            'always-semicolon': true
+        });
+        var string = '#a {#b {} #d {}}';
+        assert.equal(this.comb.processString(string, { syntax: 'less' }), string);
+    });
+});
