f953f19c09c71efea34faea1f85739ebbd2a4f65
Tony Ganch <tonyganch@gmail.com>
2015-01-31 21:26:56 +0300

    [gpe] Update `space-after-colon`

--- test/options/space-after-colon-scss/test.js
+++ test/options/space-after-colon-scss/test.js
@@ -1,4 +1,4 @@
-describe.skip('options/space-after-colon (scss):', function() {
+describe('options/space-after-colon (scss):', function() {
     it('Space after colon should not affect pseudo elements', function() {
         this.comb.configure({ 'space-after-colon': 1 });
         this.shouldBeEqual('pseudo-elements.scss', 'pseudo-elements.expected.scss');
