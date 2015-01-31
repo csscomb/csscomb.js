7b225298241134be2b92dfc5308d8bbe978f3861
Tony Ganch <tonyganch@gmail.com>
2015-01-31 21:26:56 +0300

    [gpe] Update `space-after-selector-delimiter`

--- test/options/space-after-selector-delimiter-sass/test.js
+++ test/options/space-after-selector-delimiter-sass/test.js
@@ -1,4 +1,4 @@
-describe.skip('options/space-after-selector-delimiter (sass):', function() {
+describe('options/space-after-selector-delimiter (sass):', function() {
     it('Issue 238', function() {
         this.comb.configure({ 'space-after-selector-delimiter': '\n' });
         this.shouldBeEqual('issue-238.sass', 'issue-238.expected.sass');
