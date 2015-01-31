104d074e142a04e060132fde25dfd312fa6cf1ed
Tony Ganch <tonyganch@gmail.com>
2015-01-31 21:26:56 +0300

    [gpe] Update `space-before-colon`

--- test/options/space-before-colon-sass/test.js
+++ test/options/space-before-colon-sass/test.js
@@ -1,4 +1,4 @@
-describe.skip('options/space-before-colon-sass:', function() {
+describe('options/space-before-colon-sass:', function() {
     it('Should correct space', function() {
         this.comb.configure({ 'space-before-colon': 1 });
         this.shouldBeEqual('test.sass', 'test.expected.sass');
