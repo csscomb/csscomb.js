da18af60b56e5ce068429dfb6b4f3a1ce560a030
Tony Ganch <tonyganch@gmail.com>
2015-06-09 23:16:46 +0300

    Fix #394: unitless-zero vs fractions
    Don't remove units from values starting from zero, like `0.5em`

---
+++ test/options/unitless-zero/test.js
@@ -0,0 +1,115 @@
+var assert = require('assert');
+
+describe('options/unitless-zero', function() {
+    describe('process', function() {
+        it('Should remove units in zero-valued dimensions', function() {
+            this.comb.configure({ 'unitless-zero': true });
+            return this.comb.processString(
+                'div { margin: 0em; padding: 0px }'
+            ).then(function(actual) {
+                assert.equal(actual, 'div { margin: 0; padding: 0 }');
+            });
+        });
+
+        it('Should remove units in zero-valued dimensions, test 2', function() {
+            this.comb.configure({ 'unitless-zero': true });
+            return this.comb.processString(
+                'div { margin: 0% }'
+            ).then(function(actual) {
+                assert.equal(actual, 'div { margin: 0 }');
+            });
+        });
+
+        it('Should remove units in zero-valued media-query params', function() {
+            this.comb.configure({ 'unitless-zero': true });
+            return this.comb.processString(
+                '@media all and (min-width: 0px) { div { margin: 0em; padding: 0px } }'
+            ).then(function(actual) {
+                assert.equal(actual, '@media all and (min-width: 0) { div { margin: 0; padding: 0 } }');
+            });
+        });
+
+        it('Should not remove units (degs) in rotate property', function() {
+            this.comb.configure({ 'unitless-zero': true });
+            return this.comb.processString(
+                'div { -webkit-transform: rotate(0deg); }'
+            ).then(function(actual) {
+                assert.equal(actual, 'div { -webkit-transform: rotate(0deg); }');
+            });
+        });
+
+        it('Issue 394', function() {
+            this.comb.configure({ 'unitless-zero': true });
+            this.shouldBeEqual('issue-394.css', 'issue-394.expected.css');
+        });
+    });
+
+    describe('detect', function() {
+        it('Should detect unitless zero option', function() {
+            this.shouldDetect(
+                ['unitless-zero'],
+                'a { width: 0 }',
+                {
+                    'unitless-zero': true
+                }
+            );
+        });
+
+
+        it('Should detect zero with unit', function() {
+            this.shouldDetect(
+                ['unitless-zero'],
+                'a { width: 0px }',
+                {
+                    'unitless-zero': false
+                }
+            );
+        });
+
+        it('Should detect unitless zero option with multiple values', function() {
+            this.shouldDetect(
+                ['unitless-zero'],
+                'a { padding: 0px 0 0 }',
+                {
+                    'unitless-zero': true
+                }
+            );
+        });
+
+        it('Should detect zero with unit and multiple values', function() {
+            this.shouldDetect(
+                ['unitless-zero'],
+                'a { padding: 0px 0 0em }',
+                {
+                    'unitless-zero': false
+                }
+            );
+        });
+
+        it('Shouldn’t detect unitless zero option if there is no unit', function() {
+            this.shouldDetect(
+                ['unitless-zero'],
+                'a { color: red }',
+                {}
+            );
+        });
+
+        it('Shouldn’t detect unitless zero option if there is `deg` unit', function() {
+            this.shouldDetect(
+                ['unitless-zero'],
+                'a { transform: rotate(0deg) }',
+                {}
+            );
+        });
+
+        it('Should detect unitless zero option with percents', function() {
+            this.shouldDetect(
+                ['unitless-zero'],
+                'a { padding: 0% 0 0 }',
+                {
+                    'unitless-zero': true
+                }
+            );
+        });
+    });
+});
