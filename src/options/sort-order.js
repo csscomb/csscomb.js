0b6fe156c9b3278c4b968a844dfe0b070e9e47f5
Tony Ganch <tonyganch@gmail.com>
2015-06-20 01:35:21 +0300

    [gpe] Fix `sort-order` after GPE update

--- src/options/sort-order.js
+++ src/options/sort-order.js
@@ -36,17 +36,18 @@ module.exports = {
 
     /**
      * Processes tree node.
-     * @param {node} node
-     * @param {String} syntax
+     * @param {node} ast
+     * @param {string} syntax
+     * @param {object} config
      */
-    process: function(node, syntax) {
-        var _this = this;
+    process: function(ast, syntax, config) {
         // Types of nodes that can be sorted:
         var NODES = ['atruleb', 'atruler', 'atrules', 'multilineComment',
-            'singlelineComment', 'declaration', 'space', 'include'];
+            'singlelineComment', 'declaration', 'space', 'include', 'extend'];
         // Spaces and comments:
         var SC = ['multilineComment', 'singlelineComment', 'space'];
 
+        var node;
         var currentNode;
         // Sort order of properties:
         var order = this.value;
@@ -263,141 +264,153 @@ module.exports = {
 
         // TODO: Think it through!
         // Sort properties only inside blocks:
-        if (!node.is('block')) return;
+        ast.traverseByType('block', function(block) {
+            node = block;
+            sorted = [];
+            deleted = [];
+            sc0 = [];
 
-        // Check every child node.
-        // If it is declaration (property-value pair, e.g. `color: tomato`),
-        // or @-rule (e.g. `@include nani`),
-        // combine it with spaces, semicolon and comments and move them from
-        // current node to a separate list for further sorting:
-        for (i = 0, l = node.length; i < l; i++) {
-            if (NODES.indexOf(node.get(i).type) === -1) continue;
+            // Check every child node.
+            // If it is declaration (property-value pair, e.g. `color: tomato`),
+            // or @-rule (e.g. `@include nani`),
+            // combine it with spaces, semicolon and comments and move them from
+            // current node to a separate list for further sorting:
+            for (i = 0, l = node.length; i < l; i++) {
+                if (NODES.indexOf(node.get(i).type) === -1) continue;
 
-            // Save preceding spaces and comments, if there are any, and mark
-            // them for removing from parent node:
-            sc0 = checkSC0();
-            if (!sc0) continue;
+                // Save preceding spaces and comments, if there are any,
+                // and mark them for removing from parent node:
+                sc0 = checkSC0();
+                if (!sc0) continue;
 
-            // If spaces/comments are the last nodes, stop and go to sorting:
-            if (!node.get(i)) {
-                deleted.splice(deleted.length - sc0.length, deleted.length + 1);
-                break;
+                // If spaces/comments are the last nodes, stop and go to
+                // sorting:
+                if (!node.get(i)) {
+                    deleted.splice(deleted.length - sc0.length,
+                                   deleted.length + 1);
+                    break;
+                }
+
+                // Check if the node needs to be sorted:
+                // it should be a special @-rule (e.g. `@include`) or
+                // a declaration
+                // with a valid property (e.g. `color` or `$width`).
+                // If not, proceed with the next node:
+                propertyName = null;
+                // Look for includes:
+                if (node.get(i).is('include') ||
+                    node.get(i).is('extend')) {
+                    propertyName = '$include';
+                } else {
+                    for (j = 0, nl = node.get(i).length; j < nl; j++) {
+                        currentNode = node.get(i).get(j);
+                        if (!currentNode) continue;
+
+                        if (currentNode.is('property')) {
+                            propertyName = currentNode.get(0).is('variable') ?
+                                '$variable' : currentNode.get(0).content;
+                            break;
+                        } else if (currentNode.is('atkeyword') &&
+                            currentNode.get(0).content === 'import') {
+                            // Look for imports
+                            propertyName = '$import';
+                            break;
+                        }
+                    }
+                }
+
+                // If current node is not property-value pair or import or
+                // include, skip it and continue with the next node:
+                if (!propertyName) {
+                    deleted.splice(deleted.length - sc0.length,
+                                   deleted.length + 1);
+                    continue;
+                }
+
+                // Make an extended node and move it to a separate list for
+                // further sorting:
+                sorted.push(extendNode());
             }
 
-            // Check if the node needs to be sorted:
-            // it should be a special @-rule (e.g. `@include`) or a declaration
-            // with a valid property (e.g. `color` or `$width`).
-            // If not, proceed with the next node:
-            propertyName = null;
-            // Look for includes:
-            if (node.get(i).is('include')) {
-                propertyName = '$include';
-            } else {
-                for (j = 0, nl = node.get(i).length; j < nl; j++) {
-                    currentNode = node.get(i).get(j);
-                    if (!currentNode) continue;
+            // Remove all nodes, that were moved to a `sorted` list,
+            // from parent node:
+            for (i = deleted.length - 1; i > -1; i--) {
+                node.content.splice(deleted[i], 1);
+            }
 
-                    if (currentNode.is('property')) {
-                        propertyName = currentNode.get(0).is('variable') ?
-                            '$variable' : currentNode.get(0).content;
-                        break;
-                    } else if (currentNode.is('atkeyword') &&
-                        currentNode.get(0).content === 'import') {
-                        // Look for imports
-                        propertyName = '$import';
-                        break;
+            // Sort declarations saved for sorting:
+            sorted.sort(function(a, b) {
+                // If a's group index is higher than b's group index, in
+                // a sorted list a appears after b:
+                if (a.groupIndex !== b.groupIndex)
+                    return a.groupIndex - b.groupIndex;
+
+                // If a and b belong to leftovers and `sort-order-fallback`
+                // option is set to `abc`, sort properties alphabetically:
+                if (a.groupIndex === lastGroupIndex &&
+                    config['sort-order-fallback']) {
+                    return sortLeftovers(a, b);
+                }
+
+                // If a and b have the same group index, and a's property index
+                // is higher than b's property index, in a sorted list
+                // a appears after b:
+                if (a.propertyIndex !== b.propertyIndex)
+                    return a.propertyIndex - b.propertyIndex;
+
+                // If a and b have the same group index and the same property
+                // index, in a sorted list they appear in the same order
+                // they were in original array:
+                return a.i - b.i;
+            });
+
+            // Build all nodes back together. First go sorted declarations, then
+            // everything else:
+            if (sorted.length > 0) {
+                for (i = sorted.length - 1, l = -1; i > l; i--) {
+                    currentNode = sorted[i];
+                    var prevNode = sorted[i - 1];
+                    sc0 = currentNode.sc0;
+                    var sc1 = currentNode.sc1;
+                    var sc2 = currentNode.sc2;
+
+                    sc0.reverse().map(removeEmptyLines);
+                    sc1.reverse().map(removeEmptyLines);
+                    sc2.reverse().map(removeEmptyLines);
+
+                    // Divide declarations from different groups with
+                    // an empty line:
+                    if (prevNode &&
+                        currentNode.groupIndex > prevNode.groupIndex) {
+                        if (sc0[0] && sc0[0].is('space') &&
+                           (syntax === 'sass' ||
+                            sc0[0].content.match(/\n/g) &&
+                            sc0[0].content.match(/\n/g).length < 2)) {
+                            sc0[0].content = '\n' + sc0[0].content;
+                        }
+                    }
+
+                    for (j = 0, nl = sc2.length; j < nl; j++) {
+                        node.content.unshift(sc2[j]);
+                    }
+                    if (currentNode.delim.length > 0) {
+                        var delim = syntax === 'sass' ? '\n' : ';';
+                        var declDelim = gonzales.createNode({
+                            type: 'declarationDelimiter',
+                            content: delim
+                        });
+                        node.content.unshift(declDelim);
+                    }
+                    for (j = 0, nl = sc1.length; j < nl; j++) {
+                        node.content.unshift(sc1[j]);
+                    }
+                    node.content.unshift(currentNode.node);
+
+                    for (j = 0, nl = sc0.length; j < nl; j++) {
+                        node.content.unshift(sc0[j]);
                     }
                 }
             }
-
-            // If current node is not property-value pair or import or include,
-            // skip it and continue with the next node:
-            if (!propertyName) {
-                deleted.splice(deleted.length - sc0.length, deleted.length + 1);
-                continue;
-            }
-
-            // Make an extended node and move it to a separate list for further
-            // sorting:
-            sorted.push(extendNode());
-        }
-
-        // Remove all nodes, that were moved to a `sorted` list,
-        // from parent node:
-        for (i = deleted.length - 1; i > -1; i--) {
-            node.content.splice(deleted[i], 1);
-        }
-
-        // Sort declarations saved for sorting:
-        sorted.sort(function(a, b) {
-            // If a's group index is higher than b's group index, in a sorted
-            // list a appears after b:
-            if (a.groupIndex !== b.groupIndex)
-                return a.groupIndex - b.groupIndex;
-
-            // If a and b belong to leftovers and `sort-order-fallback` option
-            // is set to `abc`, sort properties alphabetically:
-            if (a.groupIndex === lastGroupIndex &&
-                _this.getValue('sort-order-fallback')) {
-                return sortLeftovers(a, b);
-            }
-
-            // If a and b have the same group index, and a's property index is
-            // higher than b's property index, in a sorted list a appears after
-            // b:
-            if (a.propertyIndex !== b.propertyIndex)
-                return a.propertyIndex - b.propertyIndex;
-
-            // If a and b have the same group index and the same property index,
-            // in a sorted list they appear in the same order they were in
-            // original array:
-            return a.i - b.i;
         });
-
-        // Build all nodes back together. First go sorted declarations, then
-        // everything else:
-        if (sorted.length > 0) {
-            for (i = sorted.length - 1, l = -1; i > l; i--) {
-                currentNode = sorted[i];
-                var prevNode = sorted[i - 1];
-                sc0 = currentNode.sc0;
-                var sc1 = currentNode.sc1;
-                var sc2 = currentNode.sc2;
-
-                sc0.reverse().map(removeEmptyLines);
-                sc1.reverse().map(removeEmptyLines);
-                sc2.reverse().map(removeEmptyLines);
-
-                // Divide declarations from different groups with an empty line:
-                if (prevNode && currentNode.groupIndex > prevNode.groupIndex) {
-                    if (sc0[0] && sc0[0].is('space') &&
-                       (this.syntax === 'sass' ||
-                        sc0[0].content.match(/\n/g) &&
-                        sc0[0].content.match(/\n/g).length < 2)) {
-                        sc0[0].content = '\n' + sc0[0].content;
-                    }
-                }
-
-                for (j = 0, nl = sc2.length; j < nl; j++) {
-                    node.content.unshift(sc2[j]);
-                }
-                if (currentNode.delim.length > 0) {
-                    var delim = this.syntax === 'sass' ? '\n' : ';';
-                    var declDelim = gonzales.createNode({
-                        type: 'declarationDelimiter',
-                        content: delim
-                    });
-                    node.content.unshift(declDelim);
-                }
-                for (j = 0, nl = sc1.length; j < nl; j++) {
-                    node.content.unshift(sc1[j]);
-                }
-                node.content.unshift(currentNode.node);
-
-                for (j = 0, nl = sc0.length; j < nl; j++) {
-                    node.content.unshift(sc0[j]);
-                }
-            }
-        }
     }
 };
